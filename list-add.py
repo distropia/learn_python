4cec1b555c39b1462c2ae0918fc96b5304217f4c
distropia <salgadd@gmail.com>
2016-10-05 15:27:34 +0100

    create a list-add python code

---
+++ list-add.py
@@ -0,0 +1,21 @@
+from __future__ import print_function
+
+def main():
+	fruits = ["apple", "mango", "orange"]
+	print("Initial Element:")
+	print(fruits)
+
+	fruits.append("pear")
+	print("\After append():")
+	print(fruits)
+
+	fruits.insert(1, "banana")
+	print("\nAfter insert():")
+	print(fruits)
+
+	fruits.extend(["watermelon", "grape"]
+	print("\nAfter extend():")
+	print(fruits)
+
+if __name__ == "__main__";
+	main()
